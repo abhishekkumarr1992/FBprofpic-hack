ca4322ff624c856c7aa74d86f2fcc7b7f12b1900
asfaf <prakharpd@gmail.com>
2014-06-12 01:51:54 +0530

    Update test2.php
    - More simpler and optimised code
    - Better pattern matching

--- test2.php
+++ test2.php
@@ -1,26 +1,6 @@
-
 <?php
-if(isset($_POST['user']))
-{
-$a=htmlspecialchars($_POST['user']) 
-$a="https://m.facebook.com/".$a;
-$html = @file_get_contents($a)or die("unable to open");
-$dom= new DOMDocument;
-@$dom->loadHTML($html);
-$dom->preserveWhiteSpace = false;
-$images = $dom->getElementsByTagName('img');
-foreach ($images as $image) {
-  $x[]= $image->getAttribute('src');
-	
-	
-	$y= str_replace(array( 's320x320', 's480x480', 's200x200'), 's720x720', @$x[1]) ;
-	
-	
-	
-	
-}
-
-echo "<center><img src=$y  ></center>";
-}
+ $user = !empty($_POST['user'])?$_POST['user']:die("POST was empty");
+ $link = get_headers("https://graph.facebook.com/".$user."/picture?type=large",1)["Location"]or die("Connection Error");
+ $link = preg_replace("/\d{3}x\d{3}/i", "720x720", $link);
+ echo "<center><img src=\"".htmlspecialchars($link)."\"></center>";
 ?>
-
